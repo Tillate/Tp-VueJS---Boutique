e70de123e121ac0f4febb80189f0e9244587869e
allan <allan.lelous@gmail.com>
2022-04-05 18:37:33 +0200

    Ajout des evenements affichage du stock, couleurs

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
 const app = Vue.createApp ({
     data() {
         return {
+            cart: null,
             product: 'Socks',
-            description: 'Description du produit'
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            description: 'Description du produit',
+            image: './assets/images/socks_green.jpg',
+            inventory: 1,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: '#39A66F', image:'./assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: '#32455F', image:'./assets/images/socks_blue.jpg', quantity: 0 },
+            ],
+            activeClass: true
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
+        },
+    },
+    computed: {
+        title() {
+            return this.brand + '' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         }
     }
 })
